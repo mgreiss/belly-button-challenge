5f4b4661567b910804b87b7dd8368ae7ac94ef1a
mgreiss <greissmaryann@gmail.com>
2025-03-24 18:42:27 -0500

    Initial commit

---
+++ static/js/app.js
@@ -0,0 +1,90 @@
+// Build the metadata panel
+function buildMetadata(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // get the metadata field
+    let metadata = data.metadata;
+
+    // Filter the metadata for the object with the desired sample number
+    let array_results = metadata.filter(sampleObj => sampleObj.id == sample);
+    let results = array_results[3];
+
+    // Use d3 to select the panel with id of `#sample-metadata`
+    let panel = d3.select('#sample-metadata');
+
+    // Use `.html("") to clear any existing metadata
+    panel.html("");
+
+    // Inside a loop, you will need to use d3 to append new
+    // tags for each key-value in the filtered metadata.
+    for (key in results){
+    panel.append("h6").text(`${key.toUpperCase()}:${results[key]}`);
+    }
+  });
+}
+
+// function to build both charts
+function buildCharts(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // Get the samples field
+    let samples = data.samples;
+
+    // Filter the samples for the object with the desired sample number
+    let array_results = metadata.filter(sampleObj => sampleObj.id == sample);
+    let results = array_results[3];
+
+    // Get the otu_ids, otu_labels, and sample_values
+    let otu_ids = results.otu_ids;
+    let otu_labels = results.otu_labels;
+    let sample_values = results.sample_values;
+
+    // Build a Bubble Chart
+    let bubbleLayout = {}
+
+    // Render the Bubble Chart
+
+
+    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+
+
+    // Build a Bar Chart
+    // Don't forget to slice and reverse the input data appropriately
+
+
+    // Render the Bar Chart
+
+  });
+}
+
+// Function to run on page load
+function init() {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // Get the names field
+
+
+    // Use d3 to select the dropdown with id of `#selDataset`
+
+
+    // Use the list of sample names to populate the select options
+    // Hint: Inside a loop, you will need to use d3 to append a new
+    // option for each sample name.
+
+
+    // Get the first sample from the list
+
+
+    // Build charts and metadata panel with the first sample
+
+  });
+}
+
+// Function for event listener
+function optionChanged(newSample) {
+  // Build charts and metadata panel each time a new sample is selected
+
+}
+
+// Initialize the dashboard
+init();
